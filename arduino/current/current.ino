173c72de16ba776c1ccbaa6f4cfb0e64b847dbfa
natedudley <nathan.dudley@gmail.com>
2016-01-17 16:54:12 -0800

    not yet tested

---
+++ arduino/current/current.ino
@@ -0,0 +1,20 @@
+//thanks to http://openenergymonitor.org/emon/buildingblocks/arduino-sketch-current-only
+#include "EmonLib.h"                   // Include Emon Library
+EnergyMonitor emon1;                   // Create an instance
+
+void setup()
+{  
+  Serial.begin(9600);
+  
+  emon1.current(1, 111.1);             // Current: input pin, calibration.
+}
+
+void loop()
+{
+  double Irms = emon1.calcIrms(1480);  // Calculate Irms only
+  
+  Serial.print(Irms*230.0);         // Apparent power
+  Serial.print(" ");
+  Serial.println(Irms);          // Irms
+  delay(1000);
+}
