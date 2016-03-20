10f9b96b8c5eb95476dbf3fa0b4da7deb92c13bb
natedudley <nathan.dudley@gmail.com>
2016-03-20 16:46:33 -0700

    might be broken, more json configuration

--- python/tesla.py
+++ python/tesla.py
@@ -18,11 +18,8 @@ parkedDistInches = 45;
 fmt = "%Y-%m-%d %H:%M:%S %Z%z"
 startChargeTime = datetime.datetime.now()
 
-srcEmailAddr = 'joesmith@gmail.com'
-srcEmailPwd = 'supersecretpassword'
-dstEmailAddr = '4252060411@txt.att.net'
-form1 = '1_v-XDRNcJMoK46hGEg2ZAEZ_7BJm6Vbx6vY7L2UlkZZ'
-form2 = '1e8dcuI-Z1jk8mapxdnSsRfkMdEI3KonJBKlyqNCYfZZ'
+#form1 = '1_v-XDRNcJMoK46hGEg2ZAEZ_7BJm6Vbx6vY7L2UlkZZ' #total
+#form2 = '1e8dcuI-Z1jk8mapxdnSsRfkMdEI3KonJBKlyqNCYfZZ' #realTime
 
 
 def readconfig():
@@ -48,11 +45,10 @@ def calcKWHr(sumI):
 
         return kwHr
 
-def processCurrent(ser, sharedDict):
+def processCurrent(ser, sharedDict, configuration):
     global count
     global update
     global prevI
-    configuration = readconfig()
     startChargeTime = datetime.datetime.now()
     endChargeTIme = datetime.datetime.now()
     lastNagTime = datetime.datetime.min
@@ -78,7 +74,7 @@ def processCurrent(ser, sharedDict):
                 if(len(sumI) > 1):  #must be end of charge, send summary information
                     totalKwHr = calcKWHr(sumI)
                     print 'total charge was ' + str(totalKwHr)
-                    r = requests.get('http://docs.google.com/forms/d/'+form1+'/formResponse?ifq&entry.1201832211='+str(totalKwHr)+'&submit=Submit')
+                    r = requests.get('http://docs.google.com/forms/d/'+configuration['googleFormTotalKW']+'/formResponse?ifq&entry.1201832211='+str(totalKwHr)+'&submit=Submit')
                 sumI = []
                 startChargeTime = datetime.datetime.now()
 
@@ -96,7 +92,7 @@ def processCurrent(ser, sharedDict):
                 print 'send reminder'
                 server = smtplib.SMTP('smtp.gmail.com:587')
                 server.starttls()
-                server.login(srcEmailAddr,srcEmailPwd)
+                server.login(configuration['emailFromAddr'],configuration['emailFromPassword'])
                 FROM = 'Tessa'
                 TO = ['Da'] # must be a list
                 SUBJECT = ""
@@ -111,7 +107,7 @@ def processCurrent(ser, sharedDict):
 
                 print message
 
-                server.sendmail(srcEmailAddr, dstEmailAddr, message)#'From: Tessa last chrg: ' + endChargeTIme.strftime(fmt))
+                server.sendmail(configuration['emailFromAddr'], configuration['emailToAddr'], message)#'From: Tessa last chrg: ' + endChargeTIme.strftime(fmt))
                 server.quit()
             print str(count) + '-' + str(update) + ' '  + str(I)
             if count % update == 0 or abs(I - prevI) > .5: #update google sheet every time there is a change in curren tby more than .5 amp
@@ -119,20 +115,20 @@ def processCurrent(ser, sharedDict):
 
                 kwHr = calcKWHr(sumI)
                 print 'update spreadsheet ' + str(I) + ' - ' + str(kwHr)
-                r = requests.get('http://docs.google.com/forms/d/'+form2+'/formResponse?ifq&entry.2094522101='+str(I)+'&entry.33110511='+str(kwHr)+'&submit=Submit')
+                r = requests.get('http://docs.google.com/forms/d/'+configuration['googleFormRealTimeKW']+'/formResponse?ifq&entry.2094522101='+str(I)+'&entry.33110511='+str(kwHr)+'&submit=Submit')
                 #print r.text
             prevI = I
         except Exception, e:
             traceback.print_exc()
 
-def sendTxt():
+def sendTxt(fromEmail, password, toEmail):
     server = smtplib.SMTP('smtp.gmail.com:587')
     server.starttls()
-    server.login(srcEmailAddr,srcEmailPwd)
+    server.login(fromEmail,password)
     FROM = 'Tessa'
     TO = ['Da'] # must be a list
     SUBJECT = ""
-    TEXT = 'last chrg: ' + endChargeTIme.strftime(fmt)
+    TEXT = 'time to charge' #'last chrg: ' + endChargeTIme.strftime(fmt)
     # Prepare actual message
 
     message = """From: %s To: %s
@@ -143,10 +139,10 @@ Subject: Time to Charge!%s
 
     print message
 
-    server.sendmail(srcEmailAddr, dstEmailAddr, message)#'From: Tessa last chrg: ' + endChargeTIme.strftime(fmt))
+    server.sendmail(fromEmail, toEmail, message)#'From: Tessa last chrg: ' + endChargeTIme.strftime(fmt))
     server.quit()
 
-def processProximity(ser, sharedDict):
+def processProximity(ser, sharedDict, configuration):
     keepLooping = True
     parkCount = 0
 
@@ -177,7 +173,7 @@ def processProximity(ser, sharedDict):
 
                     if(sharedDict['isParked'] and not sharedDict['wasParked']):
                         print 'need to send a reminder'
-                        sendTxt()
+                        sendTxt(configuration['emailFromAddr'], configuration['emailFromPassword'], configuration['emailToAddr'])
 
                     sharedDict['wasParked'] = True
 
@@ -215,6 +211,8 @@ def main():
     manager = Manager()
     sharedDictionay = manager.dict()
 
+    configuration = readconfig()
+
     while keepTrying:
         serial0 = serial.Serial('/dev/ttyACM0') #connection to arduino1
         serial1 = serial.Serial('/dev/ttyACM1') #connection to arduino2
@@ -228,16 +226,16 @@ def main():
 
         if countCurrent > countCurrentFail + 5: #5 good readings
             keepTrying = False
-            p1 = Process(target=processCurrent, args=(serial0,sharedDictionay,))
-            p2 = Process(target=processProximity, args=(serial1,sharedDictionay,))
+            p1 = Process(target=processCurrent, args=(serial0,sharedDictionay,configuration,))
+            p2 = Process(target=processProximity, args=(serial1,sharedDictionay,configuration,))
             p1.start()
             p2.start()
             p1.join()
             p2.join()
         elif countCurrentFail > countCurrent +5:  #5 bad readings, do a swap
             keepTrying = False
-            p1 = Process(target=processCurrent, args=(serial1,sharedDictionay,))
-            p2 = Process(target=processProximity, args=(serial0,sharedDictionay,))
+            p1 = Process(target=processCurrent, args=(serial1,sharedDictionay,configuration,))
+            p2 = Process(target=processProximity, args=(serial0,sharedDictionay,configuration,))
             p1.start()
             p2.start()
             p1.join()
